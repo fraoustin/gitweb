ecc1791bbcb6f093d05d6ab167b47789c31e82a5
ZCTMDC <zctmdc@outlook.com>
2022-08-12 16:39:46 +0800

    chmod and chgrp when addrepos

--- src/cmd/addrepos.sh
+++ src/cmd/addrepos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 DIR="/var/lib/git/"
 
 log_run(){
@@ -7,40 +8,40 @@ log_run(){
 }
 
 error(){ 
-    	echo "ERROR : parameters invalid !" >&2 
-    	exit 1 
+        echo "ERROR : parameters invalid !" >&2 
+        exit 1 
 } 
 
 usage(){ 
-    	echo "Usage: addrepos NameOfRepository" 
-    	echo "--help or -h : view help" 
+        echo "Usage: addrepos NameOfRepository" 
+        echo "--help or -h : view help" 
 } 
 
 load(){
-	REPOS=$DIR$1.git
-	if [ ! -d $REPOS ]; then
-  		mkdir $REPOS
-		cd $REPOS
-		git init --bare
-		echo "$1" > description
-		chmod -R g+ws .
+    REPOS=$DIR$1.git
+    if [ ! -d $REPOS ]; then
+        mkdir $REPOS
+        cd $REPOS
+        git init --bare
+        echo "$1" > description
+        chmod -R g+ws .
         chgrp -R nginx .
-		if [ ! -f /opt/gitweb/remote/remotes.txt ]; then
-			exit 0
-		fi
-		log_run cp /opt/gitweb/post-receive $REPOS/hooks/post-receive
-		log_run chgrp nginx $REPOS/hooks/post-receive
-		log_run chmod 0755 $REPOS/hooks/post-receive
-		log_run chmod g+ws $REPOS/hooks/post-receive
-		while read line
-		do
-			if [ -z "${line}" ];then
-				continue
-			fi
-			# eg: git remote add gitweb-test1 https://gituser:gitpassword@gitweb-test1/test.git
-			log_run git remote add ${line}/$(basename $(pwd))
-		done < /opt/gitweb/remote/remotes.txt
-	fi
+        if [ ! -f /opt/gitweb/remote/remotes.txt ]; then
+            exit 0
+        fi
+        log_run cp /opt/gitweb/post-receive $REPOS/hooks/post-receive
+        log_run chgrp nginx $REPOS/hooks/post-receive
+        log_run chmod 0755 $REPOS/hooks/post-receive
+        log_run chmod g+ws $REPOS/hooks/post-receive
+        while read line
+        do
+            if [ -z "${line}" ];then
+                continue
+            fi
+            # eg: git remote add gitweb-test1 https://gituser:gitpassword@gitweb-test1/test.git
+            log_run git remote add ${line}/$(basename $(pwd))
+        done < /opt/gitweb/remote/remotes.txt
+    fi
 }
 
 # no parameters
@@ -50,12 +51,12 @@ case "$1" in
         --help)
             usage
             ;;
-         
+        
         -h)
             usage
             ;;
-         
+        
         *)
             load $1
- 
+
 esac
